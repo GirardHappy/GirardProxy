285a7df89f70382640855fcff4174a438ac43a51
GirardHappy <69600729+GirardHappy@users.noreply.github.com>
2024-04-21 11:04:52 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,17 @@ setup(
     version = "0.9.0",
     author = "Girard",
     author_email = "girardhappy.it@gmail.com",
-    description = ("Python module that scrape retrive working proxy addresses"),
+    description = ("Python module that scrape and retrive working proxy addresses"),
     license = "MIT",
     keywords = "proxy scraper",
     packages=find_packages('srcpython setup.py sdist bdist_wheel'),
-    long_description="""A module that offer you the possibility to create an object that continuosly request a proxies address, verify if they work using multithreading and retrive to you one of them <3""",
+    long_description="""A module that offer you the possibility to create an object that continuosly request proxies addresses, verify if they work using multithreading and retrive to you one of them <3""",
+    install_requires=[
+          'requests[socks]'
+      ],
     classifiers=[
         'Development Status :: 4 - Beta',
         "Topic :: Utilities",
         "License :: OSI Approved :: MIT License"
-    ],
+    ]
 )
