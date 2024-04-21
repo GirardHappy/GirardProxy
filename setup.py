a3c01d14174e339e159c9f89777714943a0b6ca4
GirardHappy <69600729+GirardHappy@users.noreply.github.com>
2024-04-21 11:53:37 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name = "GirardProxy",
-    version = "0.9.3",
+    version = "0.9.4",
     author = "Girard",
     author_email = "girardhappy.it@gmail.com",
     description = ("Python module that scrape and retrive working proxy addresses"),
     license = "MIT",
     keywords = "proxy scraper",
-    packages=find_packages('src'),
+    packages=['src'],
     long_description="""A module that offer you the possibility to create an object that continuosly request proxies addresses, verify if they work using multithreading and retrive to you one of them <3""",
     install_requires=[
           'requests[socks]'
