8f27c572f7b606563f7a2ed992e6c63d3a58ef5f
GirardHappy <69600729+GirardHappy@users.noreply.github.com>
2024-04-21 14:02:30 +0200

    Update and rename src/GirardProxy/main.py to src/proxy.py

--- src/proxy.py
+++ src/proxy.py
@@ -1,10 +1,10 @@
 import requests,threading,time
 
-class ProxyScraper():
+class Proxy():
     __country_list = ['all','AF', 'AX', 'AL', 'DZ', 'AS', 'AD', 'AO', 'AI', 'AQ', 'AG', 'AR', 'AM', 'AW', 'AU', 'AT', 'AZ', 'BS', 'BH', 'BD', 'BB', 'BY', 'BE', 'BZ', 'BJ', 'BM', 'BT', 'BO', 'BA', 'BW', 'BV', 'BR', 'IO', 'BN', 'BG', 'BF', 'BI', 'KH', 'CM', 'CA', 'CV', 'KY', 'CF', 'TD', 'CL', 'CN', 'CX', 'CC', 'CO', 'KM', 'CG', 'CD', 'CK', 'CR', 'CI', 'HR', 'CU', 'CY', 'CZ', 'DK', 'DJ', 'DM', 'DO', 'EC', 'EG', 'SV', 'GQ', 'ER', 'EE', 'ET', 'FK', 'FO', 'FJ', 'FI', 'FR', 'GF', 'PF', 'TF', 'GA', 'GM', 'GE', 'DE', 'GH', 'GI', 'GR', 'GL', 'GD', 'GP', 'GU', 'GT', 'GG', 'GN', 'GW', 'GY', 'HT', 'HM', 'VA', 'HN', 'HK', 'HU', 'IS', 'IN', 'ID', 'IR', 'IQ', 'IE', 'IM', 'IL', 'IT', 'JM', 'JP', 'JE', 'JO', 'KZ', 'KE', 'KI', 'KR', 'KP', 'KW', 'KG', 'LA', 'LV', 'LB', 'LS', 'LR', 'LY', 'LI', 'LT', 'LU', 'MO', 'MK', 'MG', 'MW', 'MY', 'MV', 'ML', 'MT', 'MH', 'MQ', 'MR', 'MU', 'YT', 'MX', 'FM', 'MD', 'MC', 'MN', 'ME', 'MS', 'MA', 'MZ', 'MM', 'NA', 'NR', 'NP', 'NL', 'AN', 'NC', 'NZ', 'NI', 'NE', 'NG', 'NU', 'NF', 'MP', 'NO', 'OM', 'PK', 'PW', 'PS', 'PA', 'PG', 'PY', 'PE', 'PH', 'PN', 'PL', 'PT', 'PR', 'QA', 'RE', 'RO', 'RU', 'RW', 'BL', 'SH', 'KN', 'LC', 'MF', 'PM', 'VC', 'WS', 'SM', 'ST', 'SA', 'SN', 'RS', 'SC', 'SL', 'SG', 'SK', 'SI', 'SB', 'SO', 'ZA', 'GS', 'ES', 'LK', 'SD', 'SR', 'SJ', 'SZ', 'SE', 'CH', 'SY', 'TW', 'TJ', 'TZ', 'TH', 'TL', 'TG', 'TK', 'TO', 'TT', 'TN', 'TR', 'TM', 'TC', 'TV', 'UG', 'UA', 'AE', 'GB', 'US', 'UM', 'UY', 'UZ', 'VU', 'VE', 'VN', 'VG', 'VI', 'WF', 'EH', 'YE', 'ZM', 'ZW']
     
     def __init__(self,scraper_timeout=60,protocol="socks5",timeout=1000,country=['all'],ssl='all',anonimity="elite",start=True):
-        """Create setup and run the ProxyScraper\n
+        """Create setup and run the Proxy Scraper\n
         ``scraper_timeout`` int
         ``protocol`` ['all','http','socks4','socks5']\n
         ``timeout`` int\n
@@ -56,7 +56,7 @@ class ProxyScraper():
         ``ssl`` ['all','yes','no']\n
         ``anonimity`` ['elite','anonymous','transparent','all']\n
         """    
-        ProxyScraper.__checkParams(protocol,timeout,country,ssl,anonimity)
+        Proxy.__checkParams(protocol,timeout,country,ssl,anonimity)
         self.__options = {}
         self.__options['url'] = f"https://api.proxyscrape.com/v3/free-proxy-list/get?request=displayproxies&protocol={protocol}&format=json&anonymity={anonimity}&timeout={timeout}&ssl={ssl}&country={','.join(country)}"
         self.__options['protocol'] = protocol
@@ -67,23 +67,23 @@ class ProxyScraper():
 
     def __checkParams(protocol,timeout,country,ssl,anonimity):
         if not protocol in ['all','http','socks4','socks5']:
-            raise ProxyScraper.ProxyError("protocol must be ['all','http','socks4','socks5']")
+            raise Proxy.ProxyError("protocol must be ['all','http','socks4','socks5']")
 
         if not type(timeout) is int:
-            raise ProxyScraper.ProxyError("timout must be an int")
+            raise Proxy.ProxyError("timout must be an int")
 
         if not type(country) is list:
-            raise ProxyScraper.ProxyError("country must be an array of iso alpha 2 countries or ['all']")
+            raise Proxy.ProxyError("country must be an array of iso alpha 2 countries or ['all']")
 
         for c in country:
-            if not c in ProxyScraper.__country_list:
-                raise ProxyScraper.ProxyError("country must be an iso alpha 2 countries or 'all'")
+            if not c in Proxy.__country_list:
+                raise Proxy.ProxyError("country must be an iso alpha 2 countries or 'all'")
 
         if not ssl in ['all','yes','no']:
-            raise ProxyScraper.ProxyError("ssl must be ['all','yes','no']")
+            raise Proxy.ProxyError("ssl must be ['all','yes','no']")
 
         if not anonimity in ['elite','anonymous','transparent','all']:
-            raise ProxyScraper.ProxyError("anonimity must be ['elite','anonymous','transparent','all']")
+            raise Proxy.ProxyError("anonimity must be ['elite','anonymous','transparent','all']")
     
     def __proxywork(host,port,ip_data):
         try:
@@ -95,7 +95,7 @@ class ProxyScraper():
     def __requestProxy(self):
         r = requests.get(self.__options.get('url')).json()
         for prox in r.get('proxies'):
-            t = ProxyScraper.__ReturnValueThread(target=ProxyScraper.__proxywork,args=(prox.get('ip'),prox.get('port'),prox.get('ip_data')))
+            t = Proxy.__ReturnValueThread(target=Proxy.__proxywork,args=(prox.get('ip'),prox.get('port'),prox.get('ip_data')))
             t.start()
             self.__threads.append(t)
         while len(self.__threads)>0:
@@ -131,13 +131,8 @@ class ProxyScraper():
             try:
                 self.result = self._target(*self._args, **self._kwargs)
             except Exception as exc:
-                ProxyScraper.ProxyError(f'ReturnValueThread Error => {type(exc).__name__}: {exc}')
+                Proxy.ProxyError(f'ReturnValueThread Error => {type(exc).__name__}: {exc}')
 
         def join(self, *args, **kwargs):
             super().join(*args, **kwargs)
             return self.result
-
-start = time.time()
-proxy = ProxyScraper()
-print(proxy)
-proxy.stop()
