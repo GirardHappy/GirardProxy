98f49c3bbd1fa3a9c3dfa38415ad56b8b87d9927
GirardHappy <69600729+GirardHappy@users.noreply.github.com>
2024-04-21 14:01:16 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "GirardProxy",
-    version = "0.9.41",
+    version = "0.9.45",
     author = "Girard",
     author_email = "girardhappy.it@gmail.com",
     description = ("Python module that scrape and retrive working proxy addresses"),
